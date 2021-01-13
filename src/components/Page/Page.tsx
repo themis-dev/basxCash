141972fcf560f7513c0215260547528c93416e6f
chipscat <zxjing1123@163.com>
2021-01-13 23:57:43 +0800

    update

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -22,6 +22,10 @@ const StyledMain = styled.div`
   flex-direction: column;
   min-height: calc(100vh - ${props => props.theme.topBarSize * 2}px);
   padding-bottom: ${props => props.theme.spacing[5]}px;
+   @media (max-width: 768px) {
+    margin-right:20px;
+    margin-left:20px;
+  }
 `
 
 export default Page
